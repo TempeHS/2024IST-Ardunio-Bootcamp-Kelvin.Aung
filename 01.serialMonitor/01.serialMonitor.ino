2ab5bb7a4c5a488e70df2a2d4e0715ae00aa7d2f
TheFileExplorer <kelvin.aung@education.nsw.gov.au>
2023-08-04 11:37:12 +1000

    Complete activity 1

--- 01.serialMonitor/01.serialMonitor.ino
+++ 01.serialMonitor/01.serialMonitor.ino
@@ -17,10 +17,14 @@
 
 // The setup function runs once when you press reset or power the board
 void setup() {
-
+Serial.begin(9600);
+Serial.println("Serial Monitor is configured to 9600");
+Serial.println("-----------------------");
 }
 
 // The loop function runs over and over again forever
 void loop() {
-
+   while(!Serial.available());
+   String myString = Serial.readString();
+   Serial.print(myString);
 }
